932b5245237fe9dbeed0da232d7c69cec996c71b
Martin Kraft <martinkraft@gmail.com>
2018-01-26 10:00:06 -0500

    XYZ-51: Tests 'SetRolePermissionsFromConfig' against JSON from policy page.

--- app/app_test.go
+++ app/app_test.go
@@ -5,6 +5,7 @@ package app
 
 import (
 	"flag"
+	"fmt"
 	"os"
 	"testing"
 
@@ -276,6 +277,8 @@ func TestDoAdvancedPermissionsMigration(t *testing.T) {
 			model.PERMISSION_CREATE_POST.Id,
 			model.PERMISSION_EDIT_POST.Id,
 			model.PERMISSION_USE_SLASH_COMMANDS.Id,
+			model.PERMISSION_DELETE_PUBLIC_CHANNEL.Id,
+			model.PERMISSION_DELETE_PRIVATE_CHANNEL.Id,
 			model.PERMISSION_MANAGE_PRIVATE_CHANNEL_MEMBERS.Id,
 			model.PERMISSION_DELETE_POST.Id,
 		},
@@ -389,7 +392,7 @@ func TestDoAdvancedPermissionsMigration(t *testing.T) {
 	for name, permissions := range expected2 {
 		role, err := th.App.GetRoleByName(name)
 		assert.Nil(t, err)
-		assert.Equal(t, permissions, role.Permissions)
+		assert.Equal(t, permissions, role.Permissions, fmt.Sprintf("'%v' did not have expected permissions", name))
 	}
 
 	// Remove the license.
